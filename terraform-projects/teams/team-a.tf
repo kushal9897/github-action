3090a31fa5378120132233f1c387023a20352a23
Kushal Agrawal <153362678+kushal9897@users.noreply.github.com>
2025-06-06 04:10:43 +0530

    Delete terraform-projects/teams directory

--- terraform-projects/teams/team-a.tf
+++
@@ -1,7 +0,0 @@
-module "team_a_instance" {
-  source        = "../modules/ec2-instance"
-  ami           = var.ami
-  instance_type = "t2.micro"
-  name          = "team-a-instance"
-  team          = "team-a"
-}
--- terraform-projects/teams/team-b.tf
+++
@@ -1,7 +0,0 @@
-module "team_b_instance" {
-  source        = "../modules/ec2-instance"
-  ami           = var.ami
-  instance_type = "t2.micro"
-  name          = "team-b-instance"
-  team          = "team-b"
-}
